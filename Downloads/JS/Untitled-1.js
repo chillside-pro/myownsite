487498e1573f1e145942a022027fc02e8baa904e
chillside-pro <79038635+chillside-pro@users.noreply.github.com>
2021-02-14 15:08:57 +0700

    What?
    What?

---
+++ Downloads/JS/Untitled-1.js
@@ -0,0 +1,9 @@
+a = prompt('This site is adapted for: Google Chrome browser, 1920x1080p, 16:9 display, 100% browser scale. If site will not be centered, try to scale it. Enter "1" to do not show it again!')
+if (a == '1') {
+    window.location.href = "../HTML/Untitled-1-1.html"
+}
+
+else {
+    stop
+}
+ 
